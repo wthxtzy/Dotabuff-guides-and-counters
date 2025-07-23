5aa353d58d6b7dffd1aeef430548fd51b248cdb9
wthxtzy <kripkripochek228@gmail.com>
2025-07-23 17:38:06 +0500

    Add some language selection for UI

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <windows.h>
 
+
 std::string toLowercase(const std::string& str) {
     std::string result;
     for (const char& c : str) {
@@ -9,6 +10,7 @@ std::string toLowercase(const std::string& str) {
     }
     return result;
 }
+
 
 std::string addDashIfTwoWords(const std::string& name) {
     std::string result;
@@ -22,57 +24,89 @@ std::string addDashIfTwoWords(const std::string& name) {
     return result;
 }
 
-void displayMenu() {
-    std::cout << "$$$ if the hero's name consists of two words, type it separated by a space plz $$$" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "What do you want to know? (1 - current builds, 2 - counters): ";
+void displayMenu(bool isRussian) {
+    if (isRussian) {
+        std::cout << "$$$ если имя героя состоит из двух слов, введите их через пробел $$$" << std::endl;
+        std::cout << "" << std::endl;
+        std::cout << "Что вы хотите узнать? (1 - текущие билды, 2 - контргерои): ";
+    } else {
+        std::cout << "$$$ if the hero's name consists of two words, type it separated by a space plz $$$" << std::endl;
+        std::cout << "" << std::endl;
+        std::cout << "What do you want to know? (1 - current builds, 2 - counters): ";
+    }
 }
 
-void processChoice(int choice) {
+
+void processChoice(int choice, bool isRussian) {
     std::string hero;
     std::string url;
 
     switch (choice) {
     case 1:
-        std::cout << "Enter hero name: ";
+        if (isRussian) {
+            std::cout << "Введите имя героя: ";
+        } else {
+            std::cout << "Enter hero name: ";
+        }
         std::cin.ignore();
         std::getline(std::cin, hero);
         hero = addDashIfTwoWords(hero);
         url = "https://www.dotabuff.com/heroes/" + toLowercase(hero) + "/guides";
-        std::cout << "Link to current builds: " << url << std::endl;
+        if (isRussian) {
+            std::cout << "Ссылка на текущие билды: " << url << std::endl;
+        } else {
+            std::cout << "Link to current builds: " << url << std::endl;
+        }
         ShellExecuteA(NULL, "open", url.c_str(), NULL, NULL, SW_SHOWNORMAL);
-        // Code to open URL in browser
         break;
     case 2:
-        std::cout << "Enter hero name: ";
+        if (isRussian) {
+            std::cout << "Введите имя героя: ";
+        } else {
+            std::cout << "Enter hero name: ";
+        }
         std::cin.ignore();
         std::getline(std::cin, hero);
         hero = addDashIfTwoWords(hero);
         url = "https://www.dotabuff.com/heroes/" + toLowercase(hero) + "/counters";
-        std::cout << "Link to counters: " << url << std::endl;
+        if (isRussian) {
+            std::cout << "Ссылка на контргероев: " << url << std::endl;
+        } else {
+            std::cout << "Link to counters: " << url << std::endl;
+        }
         ShellExecuteA(NULL, "open", url.c_str(), NULL, NULL, SW_SHOWNORMAL);
-        // Code to open URL in browser
         break;
     default:
-        std::cout << "Invalid choice." << std::endl;
+        if (isRussian) {
+            std::cout << "Неверный выбор." << std::endl;
+        } else {
+            std::cout << "Invalid choice." << std::endl;
+        }
         return;
     }
 
     std::cout << "--------------------------" << std::endl;
-    displayMenu();
+    displayMenu(isRussian);
 
     int newChoice;
     std::cin >> newChoice;
-    processChoice(newChoice);
+    processChoice(newChoice, isRussian);
 }
 
 int main() {
-    displayMenu();
+
+    std::cout << "Choose language / Выберите язык (1 - English, 2 - Russian): ";
+    int langChoice;
+    std::cin >> langChoice;
+
+    bool isRussian = (langChoice == 2);
+
+    displayMenu(isRussian);
 
     int choice;
     std::cin >> choice;
 
-    processChoice(choice);
+    processChoice(choice, isRussian);
 
     return 0;
 }
